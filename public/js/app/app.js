ad501ee8d9f1f407ef8c2701ee15cbe0f8e5b8d1
Elisha Shapiro <elishas@sinapsa.co.il>
2014-01-31 10:16:33 +0200

    added routs for angular

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,1 +1,34 @@
+/**
+ * WDB - WebDevDB
+ * @type {module|*|test.module|details.module|.location.module|b.angular.module}
+ */
 var WDB = angular.module('WDB', []);
+
+WDB.config(function($locationProvider, $routeProvider) {
+	"use strict";
+
+	//$locationProvider.html5Mode(true);
+//
+//	$routeProvider.when('/', {
+//		templateUrl: 'cards/video-cards.html',
+//		name: 'popular',
+//		controller: 'PopularCtrl'
+//	})
+//	// Channels are renamed to: "My Subscriptions"
+//	.when('/channels', {
+//		templateUrl: 'cards/video-cards.html',
+//		name: 'channels',
+//		controller: 'ChannelsCtrl'
+//	})
+//
+//	// Thers is no menu refrance for this - you can get to it only when opening a contextual video from AO.
+//	.when('/contextual', {
+//		templateUrl: 'cards/video-cards.html',
+//		name: 'contextual',
+//		controller: 'ContextualVideoCtrl'
+//	})
+//	.otherwise({
+//		redirectTo: '/'
+//	});
+
+});
